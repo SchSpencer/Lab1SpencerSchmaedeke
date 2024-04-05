3b0c636b523c572f5fdce32ad65b800c50c76a35
Spencer Schmaedeke <166170873+SchSpencer@users.noreply.github.com>
2024-04-05 16:13:55 -0400

    Add files via upload

---
+++ Laboratory.cpp
@@ -0,0 +1,26 @@
+/*
+ * Program:  <Name of Program>
+ * Author:   <Your Name>
+ * Date:     <today's date>
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+
+	cout << "Who's on first? \n";
+
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";
+}
+	return 0;
+}
